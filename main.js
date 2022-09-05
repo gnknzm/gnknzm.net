b5897c9066bc047066fe055cdae55ba23b73800b
yanorei32 <yanorei@hotmail.co.jp>
2022-09-05 16:58:48 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+(function() {
+	// idea by https://twitter.com/Tayu0404
+
+	const date2str = d => 
+		`${d.getFullYear()}-${(d.getMonth()+1+'').padStart(2, '0')}-${(d.getDate()+'').padStart(2, '0')}`;
+
+	const DAY = 24 * 60 * 60 * 1000;
+
+	document.addEventListener('DOMContentLoaded', () => {
+		document.getElementById('dummy-date').textContent = date2str(
+			new Date(new Date().getTime() - DAY * 3)
+		);
+	});
+})();
